6d64eef95ed8c646d5fe627af46e4045eaa82836
jasonb <jason.brownlee05@gmail.com>
2010-01-10 15:55:35 +1100

    working book search

--- src/algorithm_selection/selection.rb
+++ src/algorithm_selection/selection.rb
@@ -17,7 +17,8 @@
 # How to get the number of results found for a keyword in google: http://stackoverflow.com/questions/1809976/how-to-get-the-number-of-results-found-for-a-keyword-in-google
 # Google AJAX Search API + Ruby: http://chris.mowforth.com/post/146052675/google-ajax-search-api-ruby
 # Exploring the Google AJAX Search API: http://sophsec.com/research/exploring_ajax_search.html
-# Flash and other Non-Javascript Environments http://code.google.com/apis/ajaxsearch/documentation/#fonje
+# Flash and other Non-Javascript Environments (search API): http://code.google.com/apis/ajaxsearch/documentation/#fonje
+# Flash and other Non-Javascript Environments (search docs): http://code.google.com/apis/ajaxsearch/documentation/reference.html#_intro_fonje
 # Net::HTTP http://ruby-doc.org/stdlib/libdoc/net/http/rdoc/classes/Net/HTTP.html
 # Sample code.Python. Perl. Language Ajax api. Post method. 5000. api limit http://osdir.com/ml/GoogleAJAXAPIs/2009-05/msg00118.html
 
@@ -31,6 +32,8 @@ end
 require 'json'
 require 'net/http'
 
+
+# http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=Paris%20Hilton
 def get_approx_google_web_results(keyword)
   http = Net::HTTP.new('ajax.googleapis.com', 80)
   header = {'content-type'=>'application/x-www-form-urlencoded', 'charset'=>'UTF-8'}
@@ -42,7 +45,7 @@ end
 def get_approx_google_book_results(keyword)
   http = Net::HTTP.new('ajax.googleapis.com', 80)
   header = {'content-type'=>'application/x-www-form-urlencoded', 'charset'=>'UTF-8'}
-  path = "/ajax/services/search/book?v=1.0&q=#{keyword}&rsz=small"
+  path = "/ajax/services/search/books?v=1.0&q=#{keyword}&rsz=small"
   resp, data = http.request_get(path, header)
   return JSON.parse(data)
 end
@@ -55,8 +58,10 @@ def get_results(algorithm_name)
   web_rs = get_approx_google_web_results(keyword)
   scores << web_rs['responseData']['cursor']['estimatedResultCount']
   # google book
-  # web_rs = get_approx_google_book_results(keyword)
-  #   scores << web_rs['responseData']['cursor']['estimatedResultCount']
+  web_rs = get_approx_google_book_results(keyword)
+  scores << web_rs['responseData']['cursor']['estimatedResultCount']
+  # google scholar 
+  # TODO
   
   return scores
 end
