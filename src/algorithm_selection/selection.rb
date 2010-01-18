752f260e2cef99a0d88a09be413bce8e14b9e088
Jason Brownlee <jason.brownlee05@gmail.com>
2010-01-19 06:37:33 +1100

    updated copyright

--- src/algorithm_selection/selection.rb
+++ src/algorithm_selection/selection.rb
@@ -1,7 +1,7 @@
 # selection.rb
 
 # The Clever Algorithms Project: http://www.CleverAlgorithms.com
-# (c) Copyright 2010 Jason Brownlee. All Rights Reserved. 
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
 # Description:
