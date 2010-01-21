a4b18d2115fecdffde46fe6745721875ab3d8cea
jbrownlee <jason.brownlee05@gmail.com>
2010-01-21 11:10:35 +1100

    started python

--- src/language_comparison/genetic_algorithm.py
+++ src/language_comparison/genetic_algorithm.py
@@ -4,6 +4,9 @@
 # (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
+import random
+import sys
+
 NUM_GENERATIONS = 100
 NUM_BOUTS = 3
 POP_SIZE = 100
@@ -11,3 +14,14 @@ NUM_BITS = 64
 P_CROSSOVER = 0.98
 P_MUTATION = 1.0/NUM_BITS
 HALF = 0.5
+
+def onemax(bitstring):
+	sum = 0
+	for c in bitstring:
+		if(c=='1'):
+			sum = sum + 1
+	return sum
+			
+			
+print onemax("01010010")
+
