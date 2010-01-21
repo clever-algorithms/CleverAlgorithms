27382a2564ecef9035d55374e52ad405ae57466c
jbrownlee <jason.brownlee05@gmail.com>
2010-01-21 15:16:45 +1100

    started perl

---
+++ src/language_comparison/genetic_algorithm.pl
@@ -0,0 +1,13 @@
+# Genetic Algorithm in the Perl Programming Language
+
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+NUM_GENERATIONS = 100
+NUM_BOUTS = 3
+POP_SIZE = 100
+NUM_BITS = 64
+P_CROSSOVER = 0.98
+P_MUTATION = 1.0/NUM_BITS
+HALF = 0.5
