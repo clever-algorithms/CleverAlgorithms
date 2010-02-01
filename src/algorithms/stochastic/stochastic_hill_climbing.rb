27d17379cbd2341d1c3e72f72e80dca4c1074c05
Jason Brownlee <jason.brownlee05@gmail.com>
2010-02-02 06:56:12 +1100

    implementation of hill climbing

--- src/algorithms/stochastic/stochastic_hill_climbing.rb
+++ src/algorithms/stochastic/stochastic_hill_climbing.rb
@@ -1,36 +1,41 @@
-# Stochastic Hill Climbing Algorithm in the Ruby Programming Language
+# Stochastic Hill Climbing algorithm in the Ruby Programming Language
 
 # The Clever Algorithms Project: http://www.CleverAlgorithms.com
 # (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-NUM_ITERATIONS = 100
-PROBLEM_SIZE = 2
-SEARCH_SPACE = Array.new(PROBLEM_SIZE) {|i| [-5, +5]}
+NUM_ITERATIONS = 1000
+PROBLEM_SIZE = 64
 
-# TODO TODO TODO
-
-def cost(candidate_vector)
-  return candidate_vector.inject(0) {|sum, x| sum +  (x ** 2.0)}
+def cost(bitstring)
+  return bitstring.inject(0) {|sum,x| sum = sum + ((x=='1') ? 1 : 0)}
 end
 
-def random_solution(problemSize, searchSpace)
-  return Array.new(problemSize) do |i|      
-    searchSpace[i][0] + ((searchSpace[i][1] - searchSpace[i][0]) * rand)
-  end
+def random_solution(problemSize)
+  return Array.new(problemSize){|i| (rand<0.5) ? "1" : "0"}
 end
 
-def search(numIterations, problemSize, searchSpace)
-  best = nil
+def mutation(bitstring)  
+  mutant = Array.new(bitstring)
+  pos = rand(bitstring.length)
+  mutant[pos] = (mutant[pos]=='1') ? '0' : '1'
+  return mutant
+end
+
+def search(numIterations, problemSize)
+  candidate = {}
+  candidate[:vector] = random_solution(problemSize)
+  candidate[:cost] = cost(candidate[:vector])
   numIterations.times do |iter|
-    candidate = {}
-    candidate[:vector] = random_solution(problemSize, searchSpace)
-    candidate[:cost] = cost(candidate[:vector])
-    best = candidate if best.nil? or candidate[:cost] < best[:cost]
-    puts " > iteration #{(iter+1)}, best: c=#{best[:cost]}, v=#{best[:vector].inspect}"
-  end
-  return best
+    mutant = {}
+    mutant[:vector] = mutation(candidate[:vector])
+    mutant[:cost] = cost(mutant[:vector])
+    candidate = mutant if mutant[:cost] > candidate[:cost]
+    puts " > iteration #{(iter+1)}, best: c=#{candidate[:cost]}, v=#{candidate[:vector].join}"
+    break if candidate[:cost] == PROBLEM_SIZE
+  end 
+  return candidate
 end
 
-best = search(NUM_ITERATIONS, PROBLEM_SIZE, SEARCH_SPACE)
-puts "Done. Best Solution: c=#{best[:cost]}, v=#{best[:vector].inspect}"
+best = search(NUM_ITERATIONS, PROBLEM_SIZE)
+puts "Done. Best Solution: c=#{best[:cost]}, v=#{best[:vector].join}"
