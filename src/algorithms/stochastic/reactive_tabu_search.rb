aeff73832c7a5ad009d306a74199b5c1f807ed25
jbrownlee <jason.brownlee05@gmail.com>
2010-02-19 17:24:57 +1100

    code

--- src/algorithms/stochastic/reactive_tabu_search.rb
+++ src/algorithms/stochastic/reactive_tabu_search.rb
@@ -4,16 +4,6 @@
 # (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-MAX_ITERATIONS = 100
-MAX_CANDIDATES = 50
-CYCLE_MAX = 50
-BERLIN52 = [[565,575],[25,185],[345,750],[945,685],[845,655],[880,660],[25,230],[525,1000],
- [580,1175],[650,1130],[1605,620],[1220,580],[1465,200],[1530,5],[845,680],[725,370],[145,665],
- [415,635],[510,875],[560,365],[300,465],[520,585],[480,415],[835,625],[975,580],[1215,245],
- [1320,315],[1250,400],[660,180],[410,250],[420,555],[575,665],[1150,1160],[700,580],[685,595],
- [685,610],[770,610],[795,645],[720,635],[760,650],[475,960],[95,260],[875,920],[700,500],
- [555,815],[830,485],[1170,65],[830,610],[605,625],[595,360],[1340,725],[1740,245]]
-
 def euc_2d(c1, c2)
   Math::sqrt((c1[0] - c2[0])**2.0 + (c1[1] - c2[1])**2.0).round
 end
@@ -71,7 +61,7 @@ def make_tabu(tabuList, edge, iteration)
   entry[:visited] = 1
   entry[:edge] = edge
   entry[:iteration] = iteration
-  tabuList << entry
+  tabuList.push(entry)
   return entry
 end
 
@@ -153,10 +143,28 @@ def search(cities, candidateListSize, maxIterations, cycleMax, rep, chaos, incre
         current = bestCandidate if bestCandidate[:cost]<current[:cost]
       end    
     end
+    
+    tabuList.pop while tabuList.size > tabuListSize
+    
     puts " > iteration #{(iter+1)}, best: c=#{best[:cost]}"
   end
   return best
 end
+
+
+# 
+# NOTE: remove the escape mechanism - it is a tabu search extension, i think reactive tabus search is about adaptive list size?
+# 
+
+MAX_ITERATIONS = 100
+MAX_CANDIDATES = 50
+CYCLE_MAX = 50
+BERLIN52 = [[565,575],[25,185],[345,750],[945,685],[845,655],[880,660],[25,230],[525,1000],
+ [580,1175],[650,1130],[1605,620],[1220,580],[1465,200],[1530,5],[845,680],[725,370],[145,665],
+ [415,635],[510,875],[560,365],[300,465],[520,585],[480,415],[835,625],[975,580],[1215,245],
+ [1320,315],[1250,400],[660,180],[410,250],[420,555],[575,665],[1150,1160],[700,580],[685,595],
+ [685,610],[770,610],[795,645],[720,635],[760,650],[475,960],[95,260],[875,920],[700,500],
+ [555,815],[830,485],[1170,65],[830,610],[605,625],[595,360],[1340,725],[1740,245]]
 
 best = search(BERLIN52, MAX_CANDIDATES, MAX_ITERATIONS)
 puts "Done. Best Solution: c=#{best[:cost]}, v=#{best[:vector].inspect}"
