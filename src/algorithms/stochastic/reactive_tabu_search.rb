bd241b19e1af4dc228b74142b305d1b09eefed83
Jason Brownlee <jason.brownlee05@gmail.com>
2010-02-20 18:37:23 +1100

    updated code

--- src/algorithms/stochastic/reactive_tabu_search.rb
+++ src/algorithms/stochastic/reactive_tabu_search.rb
@@ -22,10 +22,37 @@ def random_permutation(cities)
   return Array.new(all.length) {|i| all.delete_at(rand(all.length))}
 end
 
-def generate_initial_solution(cities)
+# def generate_initial_solution(cities)
+#   best = {}
+#   best[:vector] = random_permutation(cities)
+#   best[:cost] = cost(best[:vector], cities)
+#   return best
+# end
+
+def stochastic_two_opt(permutation)
+  perm = Array.new(permutation)
+  c1, c2 = rand(perm.length), rand(perm.length)
+  c2 = rand(perm.length) while c1 == c2
+  c1, c2 = c2, c1 if c2 < c1
+  perm[c1...c2] = perm[c1...c2].reverse
+  return perm, [[permutation[c1-1], permutation[c1]], [permutation[c2-1], permutation[c2]]]
+end
+
+def generate_initial_solution(cities, maxNoImprovements)
   best = {}
   best[:vector] = random_permutation(cities)
   best[:cost] = cost(best[:vector], cities)
+  noImprovements = 0
+  begin
+    candidate = {}
+    candidate[:vector] = stochastic_two_opt(best[:vector])[0]
+    candidate[:cost] = cost(candidate[:vector], cities)
+    if candidate[:cost] <= best[:cost]
+      noImprovements, best = 0, candidate
+    else
+      noImprovements += 1      
+    end
+  end until noImprovements >= maxNoImprovements
   return best
 end
 
@@ -40,13 +67,6 @@ def is_tabu?(edges, tabuList, iteration, prohibitionPeriod)
     end
   end
   return false
-end
-
-def bestNonTabuMove(candidates, tabuList, iteration)
-  candidates.each do |c| 
-    return c.first if !is_tabu?(c[1], tabuList, iteration)
-  end
-  return nil
 end
 
 def make_tabu(tabuList, edge, iteration)
@@ -124,10 +144,10 @@ def sort_neighbourhood(candidates, tabuList, prohibitionPeriod, iteration)
   return tabu, admissable
 end
 
-def search(cities, candidateListSize, maxIterations, increase, decrease)
-  best = generate_initial_solution(cities)
-  current = best
-  tabuList, prohibitionPeriod = [], 1
+def search(cities, maxNoImprove, candidateListSize, maxIterations, increase, decrease)
+  current = generate_initial_solution(cities, maxNoImprove)
+  best = current
+  tabuList, prohibitionPeriod = [], 10
   visitedList, avgLength, lastChange = [], 1, 0
   maxIterations.times do |iter|
     candidates = Array.new(candidateListSize) {|i| generate_candidate(current, cities)}
@@ -141,37 +161,43 @@ def search(cities, candidateListSize, maxIterations, increase, decrease)
     # make move
     if !admissable.empty?
       current, bestMoveEdges = admissable.first
-      current, bestMoveEdges = tabu.first if !tabu.empty? and tabu.first[0][:cost]<current[:cost]
+      # current, bestMoveEdges = tabu.first if !tabu.empty? and tabu.first[0][:cost]<current[:cost]
     else
-      current, bestMoveEdges =  tabu.first
+      current, bestMoveEdges = tabu.first
     end
     # updates
     make_tabu(tabuList, bestMoveEdges, iter)
+    puts "tabuList=#{tabuList.length}, prohibitionPeriod=#{prohibitionPeriod}"
     best = candidates.first[0] if candidates.first[0][:cost] < best[:cost]
-    # memory based reaction
-    candidateEntry = get_candidate_entry(visitedList, current[:vector])
-    if !candidateEntry.nil?
-      repetitionInterval = iter - candidateEntry[:iteration]
-      candidateEntry[:iteration] = iter
-      candidateEntry[:visits] += 1
-      if repetitionInterval < 2*(cities.length-1)
-        avgLength = 0.1*(iter-candidateEntry[:iteration]) + 0.9*avgLength
-        prohibitionPeriod = prohibitionPeriod*increase
-        lastChange = iter
-      end
-    else
-      store_permutation(visitedList, current[:vector], iter)
-    end
-    if iter-lastChange > avgLength
-      prohibitionPeriod = [prohibitionPeriod*decrease,1].max
-      lastChange = iter
-    end
+    
+    
+    # dear self, i'm in the process of going back to 2opt for the moves, given swap is pox!
+    
+    #     # memory based reaction
+    #     candidateEntry = get_candidate_entry(visitedList, current[:vector])
+    #     if !candidateEntry.nil?
+    #       repetitionInterval = iter - candidateEntry[:iteration]
+    #       candidateEntry[:iteration] = iter
+    #       candidateEntry[:visits] += 1
+    #       if repetitionInterval < 2*(cities.length-1)
+    #         avgLength = 0.1*(iter-candidateEntry[:iteration]) + 0.9*avgLength
+    #         prohibitionPeriod = prohibitionPeriod*increase
+    #         lastChange = iter
+    #       end
+    #     else
+    #       store_permutation(visitedList, current[:vector], iter)
+    #     end
+    #     if iter-lastChange > avgLength
+    #       prohibitionPeriod = [prohibitionPeriod*decrease,1].max
+    #       lastChange = iter
+    #     end
     puts " > iteration #{(iter+1)}, best: c=#{best[:cost]}"
   end
   return best
 end
 
 MAX_ITERATIONS = 100
+MAX_NO_IMPROVE = 50
 MAX_CANDIDATES = 50
 INCREASE = 1.1
 DECREASE = 0.9
@@ -182,5 +208,5 @@ BERLIN52 = [[565,575],[25,185],[345,750],[945,685],[845,655],[880,660],[25,230],
  [685,610],[770,610],[795,645],[720,635],[760,650],[475,960],[95,260],[875,920],[700,500],
  [555,815],[830,485],[1170,65],[830,610],[605,625],[595,360],[1340,725],[1740,245]]
 
-best = search(BERLIN52, MAX_CANDIDATES, MAX_ITERATIONS, INCREASE, DECREASE)
+best = search(BERLIN52, MAX_NO_IMPROVE, MAX_CANDIDATES, MAX_ITERATIONS, INCREASE, DECREASE)
 puts "Done. Best Solution: c=#{best[:cost]}, v=#{best[:vector].inspect}"
