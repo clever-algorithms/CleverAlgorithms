30591271bbc4e8bfe5ffb1c61a26ecc57370b805
jbrownlee <jason.brownlee05@gmail.com>
2010-02-25 15:33:50 +1100

    updated for coding standard

--- src/algorithms/stochastic/grasp.rb
+++ src/algorithms/stochastic/grasp.rb
@@ -4,16 +4,6 @@
 # (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-MAX_ITERATIONS = 50
-LOCAL_SEARCH_NO_IMPROVEMENTS = 100
-GREEDINESS_FACTOR = 0.3
-BERLIN52 = [[565,575],[25,185],[345,750],[945,685],[845,655],[880,660],[25,230],[525,1000],
- [580,1175],[650,1130],[1605,620],[1220,580],[1465,200],[1530,5],[845,680],[725,370],[145,665],
- [415,635],[510,875],[560,365],[300,465],[520,585],[480,415],[835,625],[975,580],[1215,245],
- [1320,315],[1250,400],[660,180],[410,250],[420,555],[575,665],[1150,1160],[700,580],[685,595],
- [685,610],[770,610],[795,645],[720,635],[760,650],[475,960],[95,260],[875,920],[700,500],
- [555,815],[830,485],[1170,65],[830,610],[605,625],[595,360],[1340,725],[1740,245]]
-
 def euc_2d(c1, c2)
   Math::sqrt((c1[0] - c2[0])**2.0 + (c1[1] - c2[1])**2.0).round
 end
@@ -36,18 +26,18 @@ def stochastic_two_opt(permutation)
   return perm
 end
 
-def local_search(best, cities, maxNoImprovements)
-  noImprovements = 0
+def local_search(best, cities, max_no_improvements)
+  count = 0
   begin
     candidate = {}
     candidate[:vector] = stochastic_two_opt(best[:vector])    
     candidate[:cost] = cost(candidate[:vector], cities)
     if candidate[:cost] < best[:cost]
-      noImprovements, best = 0, candidate
+      count, best = 0, candidate
     else
-      noImprovements += 1      
+      count += 1      
     end
-  end until noImprovements >= maxNoImprovements
+  end until count >= max_no_improvements
   return best
 end
 
@@ -66,16 +56,28 @@ def construct_randomized_greedy_solution(cities, alpha)
   return candidate
 end
 
-def search(cities, maxIterations, maxNoImprovementsLS, alpha)
+def search(cities, max_iterations, max_no_improvements, alpha)
   best = nil
-  maxIterations.times do |iter|
+  max_iterations.times do |iter|
     candidate = construct_randomized_greedy_solution(cities, alpha);
-    candidate = local_search(candidate, cities, maxNoImprovementsLS)
+    candidate = local_search(candidate, cities, max_no_improvements)
     best = candidate if best.nil? or candidate[:cost] < best[:cost]
-    puts " > iteration #{(iter+1)}, best: c=#{best[:cost]}"
+    puts " > iteration #{(iter+1)}, best=#{best[:cost]}"
   end
   return best
 end
 
-best = search(BERLIN52, MAX_ITERATIONS, LOCAL_SEARCH_NO_IMPROVEMENTS, GREEDINESS_FACTOR)
+max_iterations = 50
+max_no_improvements = 100
+greediness_factor = 0.3
+berlin52 = [[565,575],[25,185],[345,750],[945,685],[845,655],[880,660],[25,230],
+ [525,1000],[580,1175],[650,1130],[1605,620],[1220,580],[1465,200],[1530,5],
+ [845,680],[725,370],[145,665],[415,635],[510,875],[560,365],[300,465],
+ [520,585],[480,415],[835,625],[975,580],[1215,245],[1320,315],[1250,400],
+ [660,180],[410,250],[420,555],[575,665],[1150,1160],[700,580],[685,595],
+ [685,610],[770,610],[795,645],[720,635],[760,650],[475,960],[95,260],
+ [875,920],[700,500],[555,815],[830,485],[1170,65],[830,610],[605,625],
+ [595,360],[1340,725],[1740,245]]
+
+best = search(berlin52, max_iterations, max_no_improvements, greediness_factor)
 puts "Done. Best Solution: c=#{best[:cost]}, v=#{best[:vector].inspect}"
