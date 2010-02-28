efd8c61e159d2739d7ee12ba48b4e1e94fdbbaad
Jason Brownlee <jason.brownlee05@gmail.com>
2010-02-28 20:37:53 +1100

    updated code listing

--- src/algorithms/evolutionary/genetic_algorithm.rb
+++ src/algorithms/evolutionary/genetic_algorithm.rb
@@ -10,60 +10,54 @@ def onemax(bitstring)
   return sum
 end
 
-def tournament(population, num_bouts)
-  best = nil
-  num_bouts.times do    
-    other = population[rand(population.size)]
-    best = other if best.nil? or other[:fitness]>best[:fitness]
-  end
-  return best
+def binary_tournament(population)
+  s1, s2 = population[rand(population.size)], population[rand(population.size)]
+  return (s1[:fitness] > s2[:fitness]) ? s1 : s2
 end
 
-def mutation(bitstring, prob_mutation)
-  string = ""
-  bitstring.each_char do |bit|
-    if rand<prob_mutation
-      string << ((bit=='1') ? "0" : "1")
-    else 
-      string << "#{bit}"
-    end
+def point_mutation(bitstring, prob_mutation)
+  child = ""
+  bitstring.size.times do |i|
+    bit = bitstring[i]
+    child << ((rand()<prob_mutation) ? ((bit=='1') ? "0" : "1") : bit)
   end
-  return string
+  return child
 end
 
-def crossover(parent1, parent2, prob_crossover)
-  num_bits = parent1[:bitstring].length
-  if rand < prob_crossover
-    cut = rand(num_bits-2) + 1
-    return parent1[:bitstring][0...cut]+parent2[:bitstring][cut...num_bits], parent2[:bitstring][0...cut]+parent1[:bitstring][cut...num_bits]
+def uniform_crossover(parent1, parent2, p_crossover)
+  return ""+parent1[:bitstring] if rand()<p_crossover
+  child = ""
+  parent1[:bitstring].size.times do |i| 
+    child << ((rand()<0.5) ? parent1[:bitstring][i] : parent2[:bitstring][i])
   end
-  return ""+parent1[:bitstring], ""+parent2[:bitstring]
+  return child
+end
+
+def reproduce(selected, population_size, p_crossover, p_mutation)
+  children = []  
+  selected.each_with_index do |p1, i|    
+    p2 = (i.even?) ? selected[i+1] : selected[i-1]
+    child = {}
+    child[:bitstring] = uniform_crossover(p1, p2, p_crossover)
+    child[:bitstring] = point_mutation(child[:bitstring], p_mutation)
+    children << child
+  end
+  return children
 end
 
 def random_bitstring(num_bits)
   return (0...num_bits).inject(""){|s,i| s<<((rand<0.5) ? "1" : "0")}
 end
 
-def reproduce(population, population_size, p_crossover, p_mutation, num_bouts)
-  children = []
-  while children.size < population_size
-    p1, p2 = tournament(population, num_bouts), tournament(population, num_bouts)
-    c1, c2 = crossover(p1, p2, p_crossover)
-    m1, m2 = mutation(c1, p_mutation), mutation(c2, p_mutation)
-    children << {:bitstring=>m1, :fitness=>0}
-    children << {:bitstring=>m2, :fitness=>0} if children.size < population_size
-  end
-  return children
-end
-
-def search(max_generations, num_bits, population_size, p_crossover, p_mutation, num_bouts)
+def search(max_generations, num_bits, population_size, p_crossover, p_mutation)
   population = Array.new(population_size) do |i|
     {:bitstring=>random_bitstring(num_bits), :fitness=>0}
   end
   population.each{|c| c[:fitness] = onemax(c[:bitstring])}
   gen, best = 0, population.sort{|x,y| y[:fitness] <=> x[:fitness]}.first  
   while best[:fitness]!=num_bits and gen<max_generations
-    children = reproduce(population, population_size, p_crossover, p_mutation, num_bouts)    
+    selected = Array.new(population_size){|i| binary_tournament(population)}
+    children = reproduce(selected, population_size, p_crossover, p_mutation)    
     children.each{|c| c[:fitness] = onemax(c[:bitstring])}
     children.sort!{|x,y| y[:fitness] <=> x[:fitness]}
     best = children.first if children.first[:fitness] > best[:fitness]
@@ -74,12 +68,11 @@ def search(max_generations, num_bits, population_size, p_crossover, p_mutation, 
   return best
 end
 
-max_generations = 100
+max_generations = 500
 population_size = 100
 num_bits = 64
 p_crossover = 0.98
 p_mutation = 1.0/num_bits
-num_bouts = 2
 
-best = search(max_generations, num_bits, population_size, p_crossover, p_mutation, num_bouts)
+best = search(max_generations, num_bits, population_size, p_crossover, p_mutation)
 puts "done! Solution: f=#{best[:fitness]}, s=#{best[:bitstring]}"
