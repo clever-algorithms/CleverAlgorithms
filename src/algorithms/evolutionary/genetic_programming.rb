8a970ff29f9d927851bfc2d98f76d7140035f83a
Jason Brownlee <jason.brownlee05@gmail.com>
2010-03-06 20:34:45 +1100

    started code listing

---
+++ src/algorithms/evolutionary/genetic_programming.rb
@@ -0,0 +1,7 @@
+# Genetic Programming in the Ruby Programming Language
+
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+TODO
