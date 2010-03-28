771b728c87a4e134cc90084cea50e053c7288853
Jason Brownlee <jason.brownlee05@gmail.com>
2010-03-28 16:08:44 +1100

    basic refs

--- web/cleveralgorithms/app/helpers/algorithms_helper.rb
+++ web/cleveralgorithms/app/helpers/algorithms_helper.rb
@@ -4,7 +4,7 @@ module AlgorithmsHelper
     paragraphs = content.split("\n")
     new_content = ""
     paragraphs.each do |paragraph|
-      new_content << "<p>#{h(paragraph)}</p>"
+      new_content << "<p>#{prepare_paragraph(paragraph)}</p>"
     end
     return new_content
   end
@@ -14,10 +14,25 @@ module AlgorithmsHelper
     new_content = ""
     new_content << "<ul>"
     paragraphs.each do |paragraph|
-      new_content << "<li>#{h(paragraph)}</li>"
+      new_content << "<li>#{prepare_paragraph(paragraph)}</li>"
     end
      new_content << "</ul>"
     return new_content
+  end
+  
+  def prepare_paragraph(paragraph)    
+    parts = paragraph.split('\cite{')
+    return paragraph if parts.size == 1
+    rs = ""
+    parts.each_with_index do |s, i|
+      rs << s and next if i == 0
+      first = s.index('}')      
+      ref = s[0...first]
+      rs<<"(<a href='##{ref}'>#{ref}</a>)"
+      rs << s[first+1..s.length]
+    end
+    
+    return rs
   end
   
 end
