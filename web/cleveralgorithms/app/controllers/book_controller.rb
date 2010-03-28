7f0e7756e19ff07f6076aadf8ce472931fd1d39f
Jason Brownlee <jason.brownlee05@gmail.com>
2010-03-28 19:53:38 +1100

    added email notifications

--- web/cleveralgorithms/app/controllers/book_controller.rb
+++ web/cleveralgorithms/app/controllers/book_controller.rb
@@ -3,4 +3,21 @@ class BookController < ApplicationController
   def index
     
   end
+  
+  
+  def create
+    @email = Email.new(params[:email])
+
+    respond_to do |format|
+      if @email.save
+        flash[:notice] = "Thanks, you will be notified when the book launches."
+        format.html { redirect_to(:action=>"index") }
+        # format.xml  { render :xml => @algorithm, :status => :created, :location => @algorithm }
+      else
+        format.html { render :action => "index" }
+        # format.xml  { render :xml => @algorithm.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
 end
