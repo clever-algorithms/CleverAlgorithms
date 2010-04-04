02af14194034001ff4c082324131fe1b6507d5e3
Jason Brownlee <jason.brownlee05@gmail.com>
2010-04-04 11:33:51 +1000

    almost working gep

--- src/algorithms/evolutionary/gene_expression_programming.rb
+++ src/algorithms/evolutionary/gene_expression_programming.rb
@@ -4,75 +4,133 @@
 # (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-def onemax(bitstring)
-  sum = 0
-  bitstring.each_char {|x| sum+=1 if x=='1'}
-  return sum
-end
-
 def binary_tournament(population)
   s1, s2 = population[rand(population.size)], population[rand(population.size)]
   return (s1[:fitness] > s2[:fitness]) ? s1 : s2
 end
 
-def point_mutation(bitstring, prob_mutation)
+def point_mutation(grammar, genome, p_mutation, head_length)
   child = ""
-  bitstring.size.times do |i|
-    bit = bitstring[i]
-    child << ((rand()<prob_mutation) ? ((bit=='1') ? "0" : "1") : bit)
+  genome.each_with_index do |v,i|
+    if rand()<p_mutation
+      if (i<head_length)
+        child << grammar["FUNC"][rand(grammar["FUNC"].length)]
+      else 
+        child << grammar["TERM"][rand(grammar["TERM"].length)]
+      end
+    else 
+      child << v
+    end
   end
   return child
 end
 
 def uniform_crossover(parent1, parent2, p_crossover)
-  return ""+parent1[:bitstring] if rand()>=p_crossover
+  return ""+parent1[:genome] if rand()>=p_crossover
   child = ""
-  parent1[:bitstring].size.times do |i| 
-    child << ((rand()<0.5) ? parent1[:bitstring][i] : parent2[:bitstring][i])
+  parent1[:genome].size.times do |i| 
+    child << ((rand()<0.5) ? parent1[:genome][i] : parent2[:genome][i])
   end
   return child
 end
 
-def reproduce(selected, population_size, p_crossover, p_mutation)
+def reproduce(grammar, selected, pop_size, p_crossover, p_mutation, head_length)
   children = []  
   selected.each_with_index do |p1, i|    
     p2 = (i.even?) ? selected[i+1] : selected[i-1]
     child = {}
-    child[:bitstring] = uniform_crossover(p1, p2, p_crossover)
-    child[:bitstring] = point_mutation(child[:bitstring], p_mutation)
+    child[:genome] = uniform_crossover(p1, p2, p_crossover)
+    child[:genome] = point_mutation(grammar, child[:genome], p_mutation, head_length)
+    
+    # TODO other?
+    
     children << child
   end
   return children
 end
 
-def random_bitstring(num_bits)
-  return (0...num_bits).inject(""){|s,i| s<<((rand<0.5) ? "1" : "0")}
+def random_genome(grammar, head_length, tail_length)
+  s = ""
+  head_length.times { s<<grammar["FUNC"][rand(grammar["FUNC"].length)]}
+  tail_length.times { s<<grammar["TERM"][rand(grammar["TERM"].length)]}
+  return s
 end
 
-def search(max_generations, num_bits, population_size, p_crossover, p_mutation)
-  population = Array.new(population_size) do |i|
-    {:bitstring=>random_bitstring(num_bits)}
+def target_function(x)
+  x**4.0 + x**3.0 + x**2.0 + x
+end
+
+def cost(program, bounds)
+  errors = 0.0    
+  10.times do
+    x = bounds[0] + ((bounds[1] - bounds[0]) * rand())
+    expression = program.gsub("x", x.to_s)
+    target = target_function(x)
+    begin score = eval(expression) rescue score = 0.0/0.0 end    
+    errors += (((score.nan? or score.infinite?) ? 0.0 : score) - target).abs
+  end    
+  return errors
+end
+
+def breadth_first_mapping(genome, grammar)
+  off, queue = 0, Array.new
+  root = {}
+  root[:node] = genome[off].chr;off+=1
+  queue.push(root)
+  while !queue.empty? do    
+    current = queue.shift
+    if grammar["FUNC"].include?(current[:node])
+      current[:left] = {}
+      current[:left][:node] = genome[off].chr;off+=1 
+      queue.push(current[:left])
+      current[:right] = {}
+      current[:right][:node] = genome[off].chr;off+=1
+      queue.push(current[:right])
+    end
   end
-  population.each{|c| c[:fitness] = onemax(c[:bitstring])}
+  return root
+end
+
+def tree_to_string(exp)
+  return exp[:node] if (exp[:left].nil? and exp[:right].nil?)
+  left = tree_to_string(exp[:left])
+  right = tree_to_string(exp[:right])
+  return "(#{left} #{exp[:node]} #{right})"
+end
+
+def evaluate(candidate, grammar, bounds)
+  candidate[:expression] = breadth_first_mapping(candidate[:genome], grammar)
+  candidate[:program] = tree_to_string(candidate[:expression])
+  candidate[:fitness] = cost(candidate[:program], bounds)
+end
+
+def search(grammar, bounds, head_length, tail_length, generations, pop_size, p_crossover, p_mutation)
+  pop = Array.new(pop_size) do
+    {:genome=>random_genome(grammar, head_length, tail_length)}
+  end
+  pop.each{|c| evaluate(c, grammar, bounds)}
   gen, best = 0, population.sort{|x,y| y[:fitness] <=> x[:fitness]}.first  
-  while best[:fitness]!=num_bits and gen<max_generations
-    selected = Array.new(population_size){|i| binary_tournament(population)}
-    children = reproduce(selected, population_size, p_crossover, p_mutation)    
-    children.each{|c| c[:fitness] = onemax(c[:bitstring])}
+  generations.times do |gen|
+    selected = Array.new(pop){|i| binary_tournament(pop)}
+    children = reproduce(grammar, selected, pop_size, p_crossover, p_mutation, head_length)    
+    children.each{|c| evaluate(c, grammar, bounds)}
     children.sort!{|x,y| y[:fitness] <=> x[:fitness]}
     best = children.first if children.first[:fitness] >= best[:fitness]
-    population = children
+    pop = children
     gen += 1
-    puts " > gen #{gen}, best: #{best[:fitness]}, #{best[:bitstring]}"
+    puts " > gen=#{gen}, f=#{best[:fitness]}, g=#{best[:genome]}, b=#{best[:program]}"
   end  
   return best
 end
 
-max_generations = 100
-population_size = 100
-num_bits = 64
-p_crossover = 0.98
-p_mutation = 1.0/num_bits
+grammar = {"FUNC"=>["+","-","*","/"], "TERM"=>["x"]}
+bounds = [1, 20]
+head_length = 24
+tail_length = head_length * (2-1) + 1
+generations = 100
+pop_size = 60
+p_crossover = 0.70
+p_mutation = 2.0/(head_length+tail_length).to_f
 
-best = search(max_generations, num_bits, population_size, p_crossover, p_mutation)
-puts "done! Solution: f=#{best[:fitness]}, s=#{best[:bitstring]}"
+best = search(grammar, bounds, head_length, tail_length, generations, pop_size, p_crossover, p_mutation)
+puts "done! Solution: f=#{best[:fitness]}, g=#{best[:genome]}, b=#{best[:program]}"
