5d9ed32206afc88afe96161041eae745988df6f2
Jason Brownlee <jason.brownlee05@gmail.com>
2010-04-10 11:08:16 +1000

    working on lcs

--- src/algorithms/evolutionary/learning_classifier_system.rb
+++ src/algorithms/evolutionary/learning_classifier_system.rb
@@ -6,10 +6,8 @@
 
 def new_classifier(condition, action, gen)
   classifier = {}
-  classifier[:action] = (rand()<0.5) ? '0' : '1'
-  classifier[:condition] = (0...length).inject(""){|s,i|s<<['1','0','#'][rand(3)]}
-  
-  
+  classifier[:action] = action
+  classifier[:condition] = condition
   
   # last time used in a GA in an action set
   classifier[:last_match_time] = gen
@@ -26,6 +24,31 @@ def new_classifier(condition, action, gen)
   classifier[:action_set_size] = 1
   #num = number of micro classifiers this classifer represents
   classifier[:num_classifier] = 1
+  
+  return classifier
+end
+
+
+def copy_classifier(parent_classifier)
+  classifier = {}
+  classifier[:action] = ""+parent_classifier[:action]
+  classifier[:condition] = ""+parent_classifier[:condition]
+  
+  # last time used in a GA in an action set
+  classifier[:last_match_time] = parent_classifier[:last_match_time]
+  # p - average expected payoff
+  classifier[:prediction_estimate] = parent_classifier[:prediction_estimate]
+  # eta - estimates errors made by predictions
+  classifier[:estimated_error] = parent_classifier[:estimated_error]
+  # f - fitness
+  classifier[:fitness] = parent_classifier[:fitness]
+  
+  # exp - times used in an action set
+  classifier[:experience] = parent_classifier[:experience]
+  # as = average size of the action set size this thing has belonged to
+  classifier[:action_set_size] = parent_classifier[:action_set_size]
+  #num = number of micro classifiers this classifer represents
+  classifier[:num_classifier] = parent_classifier[:num_classifier]
   
   
   return classifier
@@ -170,34 +193,71 @@ def binary_tournament(population)
   return (s1[:fitness] > s2[:fitness]) ? s1 : s2
 end
 
-def point_mutation(bitstring, prob_mutation)
+def point_mutation(string, p_mutation)
   child = ""
-  bitstring.size.times do |i|
-    bit = bitstring[i]
-    child << ((rand()<prob_mutation) ? ((bit=='1') ? "0" : "1") : bit)
+  string.size.times do |i|
+    bit = string[i]
+    if rand() < p_mutation
+      choice = ['1','0','#'].delete(bit)
+      child << choice[rand(choice.length)]
+    else 
+      child << bit 
+    end
   end
   return child
 end
 
-def uniform_crossover(parent1, parent2, p_crossover)
-  return ""+parent1[:condition] if rand()>=p_crossover
-  child = ""
-  parent1[:condition].size.times do |i| 
-    child << ((rand()<0.5) ? parent1[:condition][i] : parent2[:condition][i])
+def mutation(classifier, p_mutation, action_set)
+  classifier[:condition] = point_mutation(classifier[:condition])
+  if rand() < p_mutation
+    new_action = nil
+    begin
+      new_action = action_set[rand(action_set.length)]
+    end until new_action != classifier[:action]
+    classifier[:action] = new_action
   end
-  return child
 end
 
-def run_genetic_algorithm(population, action_set, instance, gen, p_crossover)
+def uniform_crossover(string1, string2)
+  return Array.new(string1.length) do 
+    ((rand()<0.5) ? string1[i] : string2[i])
+  end
+end
+
+def crossover(c1, c2, p2, p2)
+  l = c1[:condition].length
+  c1[:condition] = uniform_crossover(p1[:condition], p2[:condition])
+  c2[:condition] = uniform_crossover(p1[:condition], p2[:condition]) 
+  c1[:prediction_estimate] = (p1[:prediction_estimate]+p2[:prediction_estimate])/2.0
+  c1[:estimated_error] = 0.25*(p1[:estimated_error]+p2[:estimated_error])/2.0
+  c1[:fitness] = 0.1*(p1[:fitness]+p2[:fitness])/2.0    
+  c2[:prediction_estimate] = c1[:prediction_estimate]
+  c2[:estimated_error] = c1[:estimated_error]
+  c2[:fitness] = c1[:fitness]
+end
+
+def run_genetic_algorithm(population, action_set, instance, gen, p_crossover, p_mutation, action_set)
   p1, p2 = binary_tournament(action_set), binary_tournament(action_set)
-  condition1,  = uniform_crossover(p1, p2, p_crossover)
-  condition2 = uniform_crossover(p2, p1, p_crossover)  
-  c1 = new_classifier(condition1, ""p1[:action], gen)
-  c2 = new_classifier(condition2, ""p2[:action], gen)
+  c1, c2 = copy_classifier(p1), copy_classifier(p2)
+  crossover(c1, c2, p2, p2) if rand() < p_crossover
   
+  [c1,c2].each do |c|
+    mutation(c, p_mutation, action_set)
+    
+    
+    if subsumption?
+      # subsumption
+    else
+      # do insert
+      population << c
+    end    
+    
+    # do deletion
+    
+  end  
 end
 
-def search(length, pop_size, max_generations, action_set, p_explore, learning_rate, min_error, alpha, v, ga_frequency, p_crossover)  
+def search(length, pop_size, max_generations, action_set, p_explore, learning_rate, min_error, alpha, v, ga_frequency, p_crossover, p_mutation)  
   population = []
   max_generations.times do |gen|
     instance = generate_problem_string(length)
@@ -211,10 +271,10 @@ def search(length, pop_size, max_generations, action_set, p_explore, learning_ra
     update_fitness(action_set, min_error, learning_rate, alpha, v)
     # do subsumption ???
     
-    # run ga
+
     if can_run_genetic_algorithm(action_set, gen, ga_frequency)
       action_set.each do {|c| c[:last_match_time] = gen}
-      run_genetic_algorithm(population, action_set, instance, p_crossover)
+      run_genetic_algorithm(population, action_set, instance, p_crossover, p_mutation, action_set)
     end
     
     
@@ -234,7 +294,7 @@ min_error = 0.1
 alpha = 0.1
 v = 5
 p_crossover = 0.95
-p_mutation = 1.0/problem_size
+p_mutation = 1.0/(problem_size + 1)
 
 #probability for exploration - look it up.
 p_explore = 0.1
@@ -244,13 +304,10 @@ pop_size = 100
 ga_frequency = 10
 
 population = search(problem_size, pop_size, max_generations, action_set, p_explore, 
-  learning_rate, min_error, alpha, v, ga_frequency)
+  learning_rate, min_error, alpha, v, ga_frequency, p_crossover, p_mutation)
 puts "done! Solution: "
 
 
 # not decided yet
 discount_factor = 0
-
-p_crossover = 0.98
-p_mutation = 1.0/problem_size
 p_deletion = 0
