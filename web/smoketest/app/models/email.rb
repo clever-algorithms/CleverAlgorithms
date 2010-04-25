da36bf5bcf637d71b2b7205873e8b47fb53b27dc
Jason Brownlee <jason.brownlee05@gmail.com>
2010-04-25 16:10:59 +1000

    dropped smoke test

--- web/smoketest/app/models/email.rb
+++
@@ -1,8 +0,0 @@
-class Email < ActiveRecord::Base
-  
-  # validation
-  validates_format_of :email, :with=> /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on=>:save, :message=>"Please provide a valid email address."
-
-
-  
-end
