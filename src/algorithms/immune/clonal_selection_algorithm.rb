0ba2b28f0908ef6249b47ce2d0f870a050e5c3e0
Jason Brownlee <jason.brownlee05@gmail.com>
2010-05-15 10:07:35 +1000

    filled out clonal selection techreport

--- src/algorithms/immune/clonal_selection_algorithm.rb
+++ src/algorithms/immune/clonal_selection_algorithm.rb
@@ -4,10 +4,6 @@
 # (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-# The Clever Algorithms Project: http://www.CleverAlgorithms.com
-# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
-# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
-
 BITS_PER_PARAM = 16
 
 def objective_function(vector)
