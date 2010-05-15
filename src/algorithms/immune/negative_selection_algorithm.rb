0887188b48567dabea74e468033317a2851c8672
Jason Brownlee <jason.brownlee05@gmail.com>
2010-05-15 14:16:31 +1000

    stated implementation of negative selection

---
+++ src/algorithms/immune/negative_selection_algorithm.rb
@@ -0,0 +1,5 @@
+# Negative Selection Algorithm in the Ruby Programming Language
+
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
