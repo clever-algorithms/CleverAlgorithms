b9a42018d99220c4a86dc0e9b44f861b74196a68
Jason Brownlee <jason.brownlee05@gmail.com>
2010-07-21 20:51:26 +1000

    started optainet algorithm

---
+++ src/algorithms/immune/optainet.rb
@@ -0,0 +1,6 @@
+# Optimization Artificial Immune Network (opt-aiNet) in the Ruby Programming Language
+
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
