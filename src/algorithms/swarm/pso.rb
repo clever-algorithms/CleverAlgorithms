a54340d74df807eb3abcf477261134acf65c884c
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-14 13:59:22 +1100

    started pso code

--- src/algorithms/swarm/pso.rb
+++ src/algorithms/swarm/pso.rb
@@ -14,8 +14,36 @@ def random_vector(problem_size, search_space)
   end
 end
 
+def create_particle(problem_size, search_space)
+  particle = {}
+  particle[:position] = random_vector(problem_size, search_space)
+  particle[:cost] = objective_function(particle[:position])
+  particle[:b_position] = particle[:position]
+  particle[:b_cost] = particle[:cost]
+  particle[:velocity] = Array.new(problem_size){rand() * 1.0}
+  return particle
+end
 
+def search(max_gens, problem_size, search_space, pop_size)
+  pop = Array.new(pop_size) {create_particle(problem_size, search_space)}
+  best = pop.sort{|x,y| x[:cost] <=> y[:cost]}.first  
+  max_gens.times do |gen|
+    
+    
+    pop.sort!{|x,y| x[:cost] <=> y[:cost]}
+    best = pop.first if pop.first[:cost] < best[:cost]
+    puts " > gen #{gen+1}, fitness=#{best[:cost]}"
+  end  
+  return best
+end
 
 if __FILE__ == $0
+  problem_size = 3
+  search_space = Array.new(problem_size) {|i| [-5, +5]}
+  max_generations = 200
+  pop_size = 20
+  
 
+  best = search(max_generations, problem_size, search_space, pop_size)
+  puts "done! Solution: f=#{best[:cost]}, s=#{best[:position].inspect}"
 end
