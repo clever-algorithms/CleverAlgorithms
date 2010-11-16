2e0df1d608542beccef44cc6a6a77b96002092d3
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-17 09:11:06 +1100

    added forward propagation to implementation

--- src/algorithms/neural/backpropagation.rb
+++ src/algorithms/neural/backpropagation.rb
@@ -30,7 +30,7 @@ def generate_random_pattern(domain)
 end
 
 def initialize_weights(problem_size)
-  minmax = Array.new(problem_size + 1) {[0,0.5]}
+  minmax = Array.new(problem_size + 1) {[-0.5,0.5]}
   return random_vector(minmax)
 end
 
@@ -41,7 +41,7 @@ def update_weights(problem_size, weights, input, out_expected, output_actual, le
   weights[problem_size] += learning_rate * (out_expected - output_actual) * 1.0
 end
 
-def calculate_activation(weights, vector)
+def activate(weights, vector)
   sum = 0.0
   vector.each_with_index do |input, i|
     sum += weights[i] * input
@@ -51,46 +51,71 @@ def calculate_activation(weights, vector)
 end
 
 def transfer(activation)
-  return (activation >= 0) ? 1.0 : 0.0
+  return 1.0 / (1.0 + Math.exp(-activation)) 
 end
 
-def get_output(weights, pattern, domain)
-  activation = calculate_activation(weights, pattern[:vector])
-  out_actual = transfer(activation)
+# def transfer_derivative(transferred)
+#   return transferred * (1.0 - transferred)
+# end
+
+def forward_propagate(network, pattern, domain)
+  network.each_with_index do |layer, i|
+    input = (i==0) ? pattern[:vector] : Array.new(network[i-1].size){|k| network[i-1][k][:output]}
+    layer.each_with_index do |neuron, j|
+      neuron[:activation] = activate(neuron[:weights], input)
+      neuron[:output] = transfer(neuron[:activation])
+    end
+  end
+  out_actual = network.last[0][:output]
   out_class = domain.keys[denormalize_class_index(out_actual, domain)]
   return [out_actual, out_class]
 end
 
-def train_weights(weights, domain, problem_size, iterations, lrate)
+def create_neuron(num_inputs)
+  neuron = {}
+  neuron[:weights] = initialize_weights(num_inputs)
+  return neuron
+end
+
+def create_layer(num_neurons, num_inputs)
+  return Array.new(num_neurons){create_neuron(num_inputs)}
+end
+
+def train_network(network, domain, problem_size, iterations, lrate)
   iterations.times do |epoch|
     pattern = generate_random_pattern(domain)
-    out_v, out_c = get_output(weights, pattern, domain)    
+    out_v, out_c = forward_propagate(network, pattern, domain)    
     puts "> train got=#{out_v}(#{out_c}), exp=#{pattern[:class_norm]}(#{pattern[:class_label]})"    
-    update_weights(problem_size, weights, pattern[:vector], pattern[:class_norm], out_v, lrate)
+    # update_weights(problem_size, weights, pattern[:vector], pattern[:class_norm], out_v, lrate)
   end
 end
 
-def test_weights(weights, domain)
+def test_network(network, domain)
   correct = 0
   100.times do 
     pattern = generate_random_pattern(domain)
-    out_v, out_c = get_output(weights, pattern, domain)
+    out_v, out_c = forward_propagate(network, pattern, domain)
     correct += 1 if out_c == pattern[:class_label]
   end
-  puts "Finished test with a score of #{correct}/#{100} (#{(correct/100)*100}%)"
+  puts "Finished test with a score of #{correct}/#{100} (#{correct}%)"
 end
 
-def run(domain, problem_size, iterations, learning_rate)  
-  weights = initialize_weights(problem_size)
-  train_weights(weights, domain, problem_size, iterations, learning_rate)
-  test_weights(weights, domain)
+def run(domain, problem_size, iterations, hidden_layer_size, learning_rate)  
+  network = []
+  network << create_layer(problem_size, problem_size)
+  network << create_layer(hidden_layer_size, problem_size)
+  network << create_layer(1, hidden_layer_size)
+  
+  train_network(network, domain, problem_size, iterations, learning_rate)  
+  test_network(network, domain)
 end
 
 if __FILE__ == $0
   problem_size = 2
   domain = {"A"=>[[0,0.4999999],[0,0.4999999]],"B"=>[[0.5,1],[0.5,1]]}
   learning_rate = 0.1
+  hidden_layer_size = 2
   iterations = 60
 
-  run(domain, problem_size, iterations, learning_rate)
+  run(domain, problem_size, iterations, hidden_layer_size, learning_rate)
 end
