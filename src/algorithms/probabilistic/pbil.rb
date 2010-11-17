60f4e7adba83a99e89bc4739a679d1977f3e9972
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-18 08:35:45 +1100

    started pbil

---
+++ src/algorithms/probabilistic/pbil.rb
@@ -0,0 +1,14 @@
+# Probabilistic-Based Incremental Learning in the Ruby Programming Language
+
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+# TODO
+
+if __FILE__ == $0
+  
+
+  
+  
+end
