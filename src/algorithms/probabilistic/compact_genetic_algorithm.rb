b145027704e06f832493fd98b760445cf0190e5c
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-18 15:24:10 +1100

    released the compact genetic algorithm techreport

--- src/algorithms/probabilistic/compact_genetic_algorithm.rb
+++ src/algorithms/probabilistic/compact_genetic_algorithm.rb
@@ -18,19 +18,19 @@ def generate_candidate(vector)
   return candidate
 end
 
-def update_vector(num_bits, vector, winner, loser)
-  num_bits.times do |i|  
+def update_vector(vector, winner, loser, population_size)
+  vector.length.times do |i|  
     if winner[:bitstring][i] != loser[:bitstring][i]
       if winner[:bitstring][i] == 1
-        vector[i] = vector[i] + 1.0/num_bits.to_f
+        vector[i] = vector[i] + 1.0/population_size.to_f
       else 
-        vector[i] = vector[i] - 1.0/num_bits.to_f
+        vector[i] = vector[i] - 1.0/population_size.to_f
       end
     end
   end
 end
 
-def search(num_bits, max_iterations)
+def search(num_bits, max_iterations, population_size)
   vector = Array.new(num_bits){0.5}
   best = nil
   max_iterations.times do |iter|
@@ -38,7 +38,7 @@ def search(num_bits, max_iterations)
     c2 = generate_candidate(vector)
     winner, loser = (c1[:cost] > c2[:cost] ? [c1,c2] : [c2,c1])
     best = winner if best.nil? or winner[:cost]>best[:cost]
-    update_vector(num_bits, vector, winner, loser)
+    update_vector(vector, winner, loser, population_size)
     puts " >iteration=#{iter}, f=#{best[:cost]}, s=#{best[:bitstring]}"
   end
   return best
@@ -46,8 +46,9 @@ end
 
 if __FILE__ == $0
   num_bits = 32
-  max_iterations = 200
+  max_iterations = 130
+  population_size = 20
   
-  best = search(num_bits, max_iterations)
+  best = search(num_bits, max_iterations, population_size)
   puts "done! Solution: f=#{best[:cost]}/#{num_bits}, s=#{best[:bitstring]}"
 end
