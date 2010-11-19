2237a9d6722d41174601704ac1ead1bf889879b6
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-20 08:08:00 +1100

    more work on dca

--- src/algorithms/immune/dendritic_cell_algorithm.rb
+++ src/algorithms/immune/dendritic_cell_algorithm.rb
@@ -16,64 +16,77 @@ def construct_pattern(class_label, domain, p_safe, p_danger)
   pattern = {}
   pattern[:class_label] = class_label
   pattern[:input] = set[selection]
-  pattern[:safe] = (rand() * p_safe * 100.0)
-  pattern[:danger] = (rand() * p_danger * 100.0)  
+  pattern[:safe] = (rand() * p_safe)
+  pattern[:danger] = (rand() * p_danger)  
   return pattern
 end
 
 def generate_pattern(domain, p_anomaly, p_normal)
-  pattern = nil
-  total = domain.keys.inject(0.0){|s,k| s+domain[k].size.to_f}
-  if rand() < (domain["Anomaly"].size.to_f/total)
+  pattern = nil  
+  if rand() < 0.5
     pattern = construct_pattern("Anomaly", domain, 1.0-p_normal, p_anomaly)
+    puts ">Generated Anomoly [#{pattern[:input]}]"
   else
     pattern = construct_pattern("Normal", domain, p_normal, 1.0-p_anomaly)
   end
   return pattern
 end
 
-def initialize_cell(cell={})
+def initialize_cell(thresh, cell={})
   cell[:lifespan] = 100.0
   cell[:k] = 0.0
   cell[:cms] = 0.0
+  cell[:migration_threshold] = thresh[0] + ((thresh[1]-thresh[0]) * rand()) 
+  cell[:antigen] = {}
   return cell
 end
 
-def expose_cell(cell, pattern)
-  cms = (pattern[:safe] + pattern[:danger]) 
-  k = pattern[:danger] - (pattern[:safe] * 2.0)  
+def store_antigen(cell, input)
+  if cell[:antigen][input].nil?
+    cell[:antigen][input] = 1
+  else
+    cell[:antigen][input] += 1
+  end
+end
+
+def expose_cell(cell, cms, k, pattern, threshold)
   cell[:cms] += cms
   cell[:k] += k
-  cell[:lifespan] -= cms
+  cell[:lifespan] -= cms 
+  store_antigen(cell, pattern[:input]) 
+  if cell[:lifespan] <= 0
+    initialize_cell(threshold, cell)
+  end
 end
 
 def can_cell_migrate?(cell)
-  return cell[:lifespan] <= 0.0
+  return (cell[:cms]>=cell[:migration_threshold] and !cell[:antigen].empty?)
 end
 
-def expose_all_cells(cells, pattern)
+def expose_all_cells(cells, pattern, threshold)
   migrate = []
+  # store_antigen(cells[rand(cells.size)], pattern[:input])
+  cms = (pattern[:safe] + pattern[:danger]) 
+  k = pattern[:danger] - (pattern[:safe] * 2.0)  
   cells.each do |cell|
-    expose_cell(cell, pattern)
+    expose_cell(cell, cms, k, pattern, threshold)
     if can_cell_migrate?(cell)
       migrate << cell
       cell[:class_label] = (cell[:k]>0) ? "Anomaly" : "Normal"
-      cell[:input] = pattern[:input]
-      # puts " > Migrated in=#{pattern[:input]}, class=#{cell[:class_label]}"
     end
   end
   return migrate
 end
 
-def train_system(domain, max_iter, num_cells, p_anomaly, p_normal)
-  immature_cells = Array.new(num_cells){ initialize_cell() }
+def train_system(domain, max_iter, num_cells, p_anomaly, p_normal, threshold)
+  immature_cells = Array.new(num_cells){ initialize_cell(threshold) }
   migrated = []
   max_iter.times do |iter|
     pattern = generate_pattern(domain, p_anomaly, p_normal)
-    migrants = expose_all_cells(immature_cells, pattern)
+    migrants = expose_all_cells(immature_cells, pattern, threshold)
     migrants.each do |cell|
       immature_cells.delete(cell)
-      immature_cells << initialize_cell()
+      immature_cells << initialize_cell(threshold)
       migrated << cell
     end
     puts "> iter=#{iter} new=#{migrants.size}, migrated=#{migrated.size}"
@@ -81,27 +94,39 @@ def train_system(domain, max_iter, num_cells, p_anomaly, p_normal)
   return migrated
 end
 
-def classify_pattern(migrated, pattern)
-  response = {"Normal"=>0, "Anomaly"=>0}
+def classify_pattern(migrated, pattern, response_size)
+  input = pattern[:input]
+  num_cells, num_antigen = 0, 0
   migrated.each do |cell|
-    response[cell[:class_label]] += 1 if cell[:input] == pattern[:input]
+    if cell[:class_label] == "Anomoly" and !cell[:antigen][input].nil?
+      num_cells += 1
+      num_antigen += cell[:antigen][input]
+    end
   end
-  return (response["Normal"]>response["Anomaly"]) ? "Normal" : "Anomaly"
+  mcav = num_cells.to_f / num_antigen.to_f  
+  return (mcav>0.5) ? "Anomaly" : "Normal"
 end
 
-def test_system(migrated, domain, p_anomaly, p_normal)
+def test_system(migrated, domain, p_anomaly, p_normal, response_size)
   correct = 0
   100.times do
-    pattern = generate_pattern(domain, p_anomaly, p_normal)
-    class_label = classify_pattern(migrated, pattern)
-    correct += 1 if class_label == pattern[:class_label]
+    pattern = construct_pattern("Normal", domain, p_normal, 1.0-p_anomaly)
+    class_label = classify_pattern(migrated, pattern, response_size)
+    correct += 1 if class_label == "Normal"
   end
-  puts "Finished test with a score of #{correct}/#{100} (#{correct}%)"
+  puts "Finished testing Normal inputs #{correct}/#{100} (#{correct}%)"
+  correct = 0
+  100.times do
+    pattern = construct_pattern("Anomaly", domain, 1.0-p_normal, p_anomaly)
+    class_label = classify_pattern(migrated, pattern, response_size)
+    correct += 1 if class_label == "Anomaly"
+  end
+  puts "Finished testing Anomaly inputs #{correct}/#{100} (#{correct}%)"
 end
 
-def run(domain, max_iter, num_cells, p_anomaly, p_normal)  
-  migrated = train_system(domain, max_iter, num_cells, p_anomaly, p_normal)
-  test_system(migrated, domain, p_anomaly, p_normal)
+def run(domain, max_iter, num_cells, p_anomaly, p_normal, threshold, response_size)  
+  migrated = train_system(domain, max_iter, num_cells, p_anomaly, p_normal, threshold)
+  test_system(migrated, domain, p_anomaly, p_normal, response_size)
 end
 
 if __FILE__ == $0
@@ -109,11 +134,12 @@ if __FILE__ == $0
   domain["Normal"] = Array.new(50){|i| i}
   domain["Anomaly"] = Array.new(5){|i| (i+1)*10}
   domain["Normal"] = domain["Normal"] - domain["Anomaly"]
-  p_anomaly = 0.70
+  p_anomaly = 0.80
   p_normal = 0.95
-  
   iterations = 100
   num_cells = 20
+  threshold = [5,15]
+  response_size = 10
 
-  run(domain, iterations, num_cells, p_anomaly, p_normal)
+  run(domain, iterations, num_cells, p_anomaly, p_normal, threshold, response_size)
 end
