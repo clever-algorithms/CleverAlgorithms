7b6d330df190dd45f43c4115c7956c962b3a230b
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-20 16:28:34 +1100

    working on bfoa implementation

--- src/algorithms/swarm/bfoa.rb
+++ src/algorithms/swarm/bfoa.rb
@@ -14,23 +14,95 @@ def random_vector(problem_size, search_space)
   end
 end
 
-def search(max_gens, problem_size, search_space, pop_size)
-  pop = Array.new(pop_size) { {:vector=>random_vector(problem_size, search_space)} }
-  pop.each{|c| c[:fitness] = objective_function(c[:vector])}
-  best = pop.sort{|x,y| x[:fitness]<=>y[:fitness]}.first
-  max_gens.times do |iter|
+def generate_random_direction(problem_size)
+  bounds = Array.new(problem_size){[-1.0,1.0]}
+  return random_vector(problem_size, bounds)
+end
 
-    puts " >iteration=#{iter}, f=#{best[:fitness]}, v=#{best[:vector]}"
-  end  
+def compute_cell_interaction(cell, cells, d, w)
+  sum = 0.0
+  cells.each do |c|
+    diff = 0.0
+    c[:vector].each_with_index do |v,i|
+      diff += (cell[:vector][i] - v)**2.0
+    end
+    sum += d * Math.exp(w * diff)
+  end
+  return sum
+end
+
+def compute_attract_repel(cell, cells, d_attr, w_attr, h_rep, w_rep)
+  attract = compute_cell_interaction(cell, cells, -d_attr, -w_attr)
+  repel = compute_cell_interaction(cell, cells, h_rep, -w_rep)
+  return attract + repel
+end
+
+def evaluate(cell, cells, d_attr, w_attr, h_rep, w_rep)
+  cell[:cost] = objective_function(cell[:vector])
+  cell[:interaction] = compute_attract_repel(cell, cells, d_attr, w_attr, h_rep, w_rep)
+  cell[:fitness] = cell[:cost] + cell[:interaction]
+end
+
+def tumble_cell(problem_size, cell, step_size)
+  step = generate_random_direction(problem_size)  
+  vector = Array.new(problem_size) do |i|
+    cell[:vector][i] + step_size * step[i]
+  end
+  return {:vector=>vector}
+end
+
+def swim_cell(cell, cells, step_size)
+  
+end
+
+
+
+def search(problem_size, search_space, pop_size, elim_disp_steps, repro_steps, chem_steps, swim_length, step_size, d_attr, w_attr, h_rep, w_rep)  
+  cells = Array.new(pop_size) { {:vector=>random_vector(problem_size, search_space)} }
+  best = nil
+  elim_disp_steps.times do |l|
+    repro_steps.times do |k|
+      chem_steps.times do |j|
+        moved_cells = []   
+        cells.each_with_index do |cell, i|
+          evaluate(cell, cells, d_attr, w_attr, h_rep, w_rep)          
+          best = cell if best.nil? or cell[:cost] < best[:cost]
+          swim_length.times do |m|
+            new_cell = tumble_cell(problem_size, cell, step_size)
+            evaluate(new_cell, cells, d_attr, w_attr, h_rep, w_rep)          
+            best = cell if cell[:cost] < best[:cost]
+            break if new_cell[:fitness] < cell[:fitness]
+            cell = new_cell
+          end
+          moved_cells << cell
+        end
+        # TODO reproduce
+      end
+      # TODO elimination-dispersal
+    end
+    
+    puts " >iteration=#{l}, fitness=#{best[:fitness]}, cost=#{best[:cost]}"
+  end
+
   return best
 end
 
 if __FILE__ == $0
   problem_size = 3
   search_space = Array.new(problem_size) {|i| [-5, 5]}
-  max_generations = 100
-  pop_size = 50
 
-  best = search(max_generations, problem_size, search_space, pop_size)
-  puts "done! Solution: f=#{best[:fitness]}, s=#{best[:vector]}"
+  pop_size = 50
+  d_attr = 0.1
+  w_attr = 0.2 
+  h_rep = d_attr
+  w_rep = 10
+  
+  step_size = 0.1
+  elim_disp_steps = 50
+  repro_steps = 3
+  chem_steps = 5
+  swim_length = 10
+
+  best = search(problem_size, search_space, pop_size, elim_disp_steps, repro_steps, chem_steps, swim_length, step_size, d_attr, w_attr, h_rep, w_rep)
+  puts "done! Solution: f=#{best[:cost]}, s=#{best[:vector].inspect}"
 end
