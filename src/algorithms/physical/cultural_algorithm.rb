9f137a799c5f20f89c28b57581e0cbb4b91e1759
Jason Brownlee <jason.brownlee05@gmail.com>
2010-11-21 08:43:23 +1100

    started the cultural algorithm

---
+++ src/algorithms/physical/cultural_algorithm.rb
@@ -0,0 +1,80 @@
+# Cultural Algorithm in the Ruby Programming Language
+
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2010 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+def onemax(bitstring)
+  sum = 0
+  bitstring.each_char {|x| sum+=1 if x=='1'}
+  return sum
+end
+
+def random_bitstring(num_bits)
+  return (0...num_bits).inject(""){|s,i| s<<((rand<0.5) ? "1" : "0")}
+end
+
+def binary_tournament(population)
+  s1, s2 = population[rand(population.size)], population[rand(population.size)]
+  return (s1[:fitness] > s2[:fitness]) ? s1 : s2
+end
+
+def point_mutation(bitstring, prob_mutation)
+  child = ""
+   bitstring.each_char do |bit|
+    child << ((rand()<prob_mutation) ? ((bit=='1') ? "0" : "1") : bit)
+  end
+  return child
+end
+
+def uniform_crossover(parent1, parent2, p_crossover)
+  return ""+parent1 if rand()>=p_crossover
+  child = ""
+  parent1.length.times do |i| 
+    child << ((rand()<0.5) ? parent1[i].chr : parent2[i].chr)
+  end
+  return child
+end
+
+def reproduce(selected, population_size, p_crossover, p_mutation)
+  children = []  
+  selected.each_with_index do |p1, i|    
+    p2 = (i.even?) ? selected[i+1] : selected[i-1]
+    child = {}
+    child[:bitstring] = uniform_crossover(p1[:bitstring], p2[:bitstring], p_crossover)
+    child[:bitstring] = point_mutation(child[:bitstring], p_mutation)
+    children << child
+    break if children.size >= population_size
+  end
+  return children
+end
+
+def search(max_generations, num_bits, population_size, p_crossover, p_mutation)
+  population = Array.new(population_size) do |i|
+    {:bitstring=>random_bitstring(num_bits)}
+  end
+  population.each{|c| c[:fitness] = onemax(c[:bitstring])}
+  gen, best = 0, population.sort{|x,y| y[:fitness] <=> x[:fitness]}.first  
+  while best[:fitness]!=num_bits and gen<max_generations
+    selected = Array.new(population_size){|i| binary_tournament(population)}
+    children = reproduce(selected, population_size, p_crossover, p_mutation)    
+    children.each{|c| c[:fitness] = onemax(c[:bitstring])}
+    children.sort!{|x,y| y[:fitness] <=> x[:fitness]}
+    best = children.first if children.first[:fitness] >= best[:fitness]
+    population = children
+    gen += 1
+    puts " > gen #{gen}, best: #{best[:fitness]}, #{best[:bitstring]}"
+  end  
+  return best
+end
+
+if __FILE__ == $0
+  max_generations = 100
+  population_size = 100
+  num_bits = 64
+  p_crossover = 0.98
+  p_mutation = 1.0/num_bits
+
+  best = search(max_generations, num_bits, population_size, p_crossover, p_mutation)
+  puts "done! Solution: f=#{best[:fitness]}, s=#{best[:bitstring]}"
+end
